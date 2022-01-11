5ab7d4a639d875992e70a6bc18d65934d42ad767
rosen <you@example.com>
2022-01-11 10:51:28 +0700

    sequelize sequelize-cli config

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,10 @@
 import express from "express";
+import homeController from "../controllers/homeController"
 
 let router = express.Router();
 
 let initWebRouter = (app) => {
-    router.get('/', (req, res) => {
-        return res.send("HELLO WORLD");
-    })
+    router.get('/', homeController.getHomePage);
     return app.use("/", router);
 }
 
